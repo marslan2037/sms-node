49986f6df81c6c89b68ab1432310c784de0b90c3
Muhammad Arslan <m.arslan2037@gmail.com>
2021-08-10 13:56:58 +0500

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 //Allow Domain
 // exports.ALLOW_DOMAIN='http://localhost:4200'; // For Development
-exports.ALLOW_DOMAIN='school.markupsoft.com'; // For Production
+exports.ALLOW_DOMAIN='http://school.markupsoft.com'; // For Production
 // exports.ALLOW_DOMAIN='http://locahost:6000'; // For Production
