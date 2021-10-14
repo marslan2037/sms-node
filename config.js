9c8ccc8f80c387173990067c8e5dbf21221cc8c0
Sajawal Waheed <sajawalwaheed31@gmail.com>
2021-10-14 23:09:11 +0500

    fileupload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 //Allow Domain
-// exports.ALLOW_DOMAIN='http://localhost:4200'; // For Development
-exports.ALLOW_DOMAIN='http://school.markupsoft.com'; // For Production
+exports.ALLOW_DOMAIN='http://localhost:4200'; // For Development
+//exports.ALLOW_DOMAIN='http://school.markupsoft.com'; // For Production
